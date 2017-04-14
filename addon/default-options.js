417b79977807615fab9442ee2a631f029f771dad
Mykola Basov <kolybasov@gmail.com>
2017-04-14 09:40:06 +0300

    Export plain arrays instead of Ember.NativeArray.

--- addon/default-options.js
+++ addon/default-options.js
@@ -1,12 +1,8 @@
-import Ember from 'ember';
-
-const { A } = Ember;
-
 // Options: https://github.com/yabwe/medium-editor/blob/master/OPTIONS.md
 export default {};
 
 // List of "first class" options that can be passed directly to component
-export const optionsList = new A([
+export const optionsList = [
   'delay', 'disableReturn', 'disableEditing', 'extensions', 'spellcheck',
   'targetBlank', 'toolbar', 'anchorPreview', 'placeholder', 'imageDragging'
-]);
+];
