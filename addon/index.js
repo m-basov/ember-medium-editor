7048efe5637dc5efec77d59aa519f003ed77c0c5
Mykola Basov <kolybasov@gmail.com>
2018-02-14 21:29:24 +0200

    Extract render-to-string component to separate addon

--- addon/index.js
+++ addon/index.js
@@ -1,18 +1,1 @@
-/**
- * ## Installation
- *
- * ```shell
- * ember install ember-medium-editor
- * ```
- *
- * ## Usage
- *
- * ```handlebars
- * {{medium-editor
- *    model.text
- *    options=(hash)
- *    onChange=(action (mut model.text))}}
- * ```
- *
- * @module Usage
- */
+export { default as MEExtensionMixin } from 'ember-medium-editor/mixins/me-extension';
--- app/components/render-to-string.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-medium-editor/components/render-to-string';
